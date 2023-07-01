7bdf0d6f5ca4f30fa2df67fbfc9f0aec26f30b73
SayNoToMustache <fktrcfylhfktrcfylh1990@gmail.com>
2023-07-01 14:25:09 +0300

    base 1.0.0

--- BASE/hlpflsht.py
+++ BASE/hlpflsht.py
@@ -115,24 +115,56 @@ class HelpHackathon:
         plt.show()
 
 
-    def moving_average(self, data, label_encoder, tiker_name = 'TICKER', column_name = 'OPEN', window_size = 1):
+    def moving_average(self, data, label_encoder, tiker_name = 'TICKER', column_name = 'CLOSE', window_size = 1):
         """СОздает в data новую колонку SMA в которой будет храниться скользящее среднее сделанное по заданному размеру окна"""
 
-        data['SMA'] = data[column_name]
+        data['SMA'] = data[column_name].copy()
+        # data['SMMA'] = data['SMA'].copy()
 
         for i, name in enumerate(self.uniq_ticker_names):
             indexes = data.loc[data[tiker_name] == name].index
             
             data.loc[indexes, 'SMA'] = data.loc[indexes, column_name].rolling(window=window_size).mean()
+            
+            #data.loc[indexes, 'SMMA'] = (data.loc[indexes, 'SMA'].shift() * (window_size-1) + data.loc[indexes, column_name]) / window_size
+
+    def calculate_smma_of_u(self, data, period, tiker_name = 'TICKER'):
+        data['U'] = data['CLOSE'].copy(deep=True)
+        data['SMA_U'] = data['CLOSE'].copy(deep=True)
+        data['SMMA_U'] = data['CLOSE'].copy(deep=True)
+
+        for i, name in enumerate(self.uniq_ticker_names):
+            indexes = data.loc[data[tiker_name] == name].index
+            data.loc[indexes, 'U'] = data.loc[indexes, 'U'].diff().apply(lambda x: x if x > 0 else 0)
+        
+            data.loc[indexes, 'SMMA_U'] = data.loc[indexes, 'U'].rolling(window=period).mean()
+            data.loc[indexes, 'SMA_U'] = data.loc[indexes, 'U'].rolling(window=period).mean()
+            data.loc[indexes, 'SMMA_U'] = data.loc[indexes, 'SMMA_U'].shift(1) * (period - 1) + data.loc[indexes, 'U']
+            data.loc[indexes, 'SMMA_U'] = data.loc[indexes, 'SMMA_U'].rolling(window=period).sum() / period
+
+    def calculate_smma_of_d(self, data, period, tiker_name = 'TICKER'):
+        data['D'] = data['CLOSE']
+        data['SMMA_D'] = data['CLOSE']
+
+        for i, name in enumerate(self.uniq_ticker_names):
+            indexes = data.loc[data[tiker_name] == name].index
+            data.loc[indexes, 'D'] = data.loc[indexes, 'D'].diff().apply(lambda x: -x if x < 0 else 0)
+        
+            data.loc[indexes, 'SMMA_D'] = data.loc[indexes, 'D'].rolling(window=period).mean()
+            data.loc[indexes, 'SMMA_D'] = data.loc[indexes, 'SMMA_D'].shift(1) * (period - 1) + data.loc[indexes, 'D']
+            data.loc[indexes, 'SMMA_D'] = data.loc[indexes, 'SMMA_D'].rolling(window=period).sum() / period
 
     def hackathon_seasonal_decompose(self, data, label_encoder, label_name = 0, tiker_name = 'TICKER', column_name = 'OPEN', period = 12):
         """Декомпозиция временных рядов"""
         from statsmodels.tsa.seasonal import seasonal_decompose
 
         print(f'Декомпозиция для {label_encoder.inverse_transform([self.uniq_ticker_names[label_name]])}')
-        # Декомпозиция временных рядов
+
         result = seasonal_decompose(data[data[tiker_name] == self.uniq_ticker_names[label_name]][column_name], model='additive', period=period)
 
-        # Визуализация декомпозиции
         result.plot()
         plt.show()
+
+    def RSI_hh(self, data):
+        data['RS'] = data['SMMA_U'] / data['SMMA_D']
+        data['RSI'] = 100 - 100 / data['RS']
